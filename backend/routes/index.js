a01b44c7ef615d4261f8680e3d9b4eebd1a49db1
rouben <rouben.ghambaryan@gmail.com>
2022-06-22 12:46:00 -0700

    frontendinitial

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,8 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.use('/api', apiRouter);
-
-router.get('/hello/world', function (req, res) {
-	res.cookie('XSRF-TOKEN', req.csrfToken());
-	res.send('Hello World!');
+router.post('/test', (req, res) => {
+	res.json({ requestBody: req.body });
 });
 
 module.exports = router;
