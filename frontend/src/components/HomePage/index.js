26156ffd9413208ed19dae0d9cbbeff44f54ef69
rouben <rouben.ghambaryan@gmail.com>
2022-06-23 20:22:41 -0700

    commentoutstartfree

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -9,20 +9,16 @@ function HomePage() {
 			<section className="home-section">
 				<div className="text-container">
 					<h2 className="home-h2">Find your inspiration. </h2>
-					<div>
-						<p className="home-p">
-							Join the Liqr community, home to tens of billions of
-							images shared by alcohol enthusiasts.
-						</p>
-					</div>
-					<div className='sff-btn'>
-						<button
-							className="button-start-for-free"
-							onClick={() => history.push('/signup')}
-						>
-							Start for free
-						</button>
-					</div>
+					<p className="home-p">
+						Join the Liqr community, home to tens of billions of images
+						shared by alcohol enthusiasts.
+					</p>
+					{/* <button
+						className="button-start-for-free"
+						onClick={() => history.push('/signup')}
+					>
+						Start for free
+					</button> */}
 				</div>
 			</section>
 		</div>
